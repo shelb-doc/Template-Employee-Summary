8020c483b0458587672271b1e69c20d0004b0cab
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-09 18:17:59 -0500

    cleaned up code

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const { inherits } = require("util");
+const { start } = require("repl");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -17,17 +18,16 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const employees = [];
 const q = new Questions
 
-async function createManager (){
-    const managerResponses = await inquirer.prompt(q.managerQuestions());
-    console.log(managerResponses);
-}
+
 
 async function init() {
     try {
-
-        await createManager();
-
-
+        const startApp = await q.startQuestion();
+        if(startApp.startGame){
+            const empData = await q.employeeQuestions();
+            console.log(empData);
+        }
+        console.log(startApp);
     } catch (error) {
         console.log(error);
     };
--- questions.js
+++ questions.js
@@ -1,38 +1,102 @@
+const inquirer = require("inquirer");
+
+
 class Questions {
-    managerQuestions() {
-        const managerQuestions = [
+
+    async startQuestion(){
+        const startQuestion = [
             {
                 type: "confirm",
                 name: "startGame",
                 message: "Welcome to Team Bulider 9000 | Here we make a Webpage that Generates *YOUR* Team! | Do you want to try this App?",
             },
+        ]
+        const startResponses = await inquirer.prompt(startQuestion);
+        return startResponses
+    }
+
+    confirmEmployee(){
+        const confirmEmployee = [
+            {
+                type: "confirm",
+                name: "confirmEmp",
+                message: "Would you like to add another Employee?",
+            },
+        ]
+        return confirmEmployee
+    }
+
+    async employeeQuestions() {
+        const employeeQuestions = [
             {
                 type: "input",
-                name: "mgrName",
-                message: "Hello!, Manager What is your name?",
+                name: "empName",
+                message: "Hello!, What is your name?",
                 default: "Mimikyu Cignetti"
             },
             {
                 type: "input",
-                name: "mgrEmail ",
+                name: "empEmail",
                 message: "What is your employee Email?",
                 default: "mimikyu@fake.com"
             },
             {
                 type: "input",
-                name: "mgrId ",
+                name: "empId",
                 message: "What is your employee ID?",
                 default: "9987"
             },
+        ]
+        const employeeResponses = await inquirer.prompt(employeeQuestions);
+        return  employeeResponses
+    }
+
+    managerQuestions() {
+        const managerQuestions = [
             {
                 type: "input",
                 name: "mgrOffice",
-                message: "What is your employee ID?",
+                message: "What is your Office Number?",
                 default: "42"
             },
         ]
         return managerQuestions
     }
+
+//     engineerQuestions() {
+//         const engineerQuestions = [
+//             {
+//                 type: "",
+//                 name: "",
+//                 message: "",
+//             },
+//             {
+//                 type: "input",
+//                 name: "mgrName",
+//                 message: "Hello!, Manager What is your name?",
+//                 default: "Mimikyu Cignetti"
+//             },
+//             {
+//                 type: "input",
+//                 name: "mgrEmail ",
+//                 message: "What is your employee Email?",
+//                 default: "mimikyu@fake.com"
+//             },
+//             {
+//                 type: "input",
+//                 name: "mgrId ",
+//                 message: "What is your employee ID?",
+//                 default: "9987"
+//             },
+//             {
+//                 type: "input",
+//                 name: "mgrOffice",
+//                 message: "What is your employee ID?",
+//                 default: "42"
+//             },
+//         ]
+//         return engineerQuestions
+//     }
 
 //     internQuestions() {
 //         const internQuestions = [
