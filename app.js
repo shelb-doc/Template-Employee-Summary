eddeb488ebb42eae0853460d61e3088f984d6cd4
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-09 17:28:26 -0500

    testing the questions"

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const render = require("./lib/htmlRenderer");
-const questions = require("./questions");
+const Questions = require("./questions");
 
 // External Packages
 const inquirer = require("inquirer");
@@ -15,9 +15,10 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const employees = [];
+const q = new Questions
 
 async function createManager (){
-    const managerResponses = await inquirer.prompt(questions.manager);
+    const managerResponses = await inquirer.prompt(q.managerQuestions());
     console.log(managerResponses);
 }
 
--- questions.js
+++ questions.js
@@ -1,14 +1,53 @@
-const managerQuestions = [
-    {
-        type: "confirm",
-        name: "startGame",
-        message: "Welcome to Team Bulider 9000 | Here we make a Webpage that Generates *YOUR* Team! | Do you want to try this App?",
-    },
-    {
-        type: "input",
-        name: "mgrName",
-        message: "Hello!, Manager What is your name?",
-    
+class Questions {
+    managerQuestions() {
+        const managerQuestions = [
+            {
+                type: "confirm",
+                name: "startGame",
+                message: "Welcome to Team Bulider 9000 | Here we make a Webpage that Generates *YOUR* Team! | Do you want to try this App?",
+            },
+            {
+                type: "input",
+                name: "mgrName",
+                message: "Hello!, Manager What is your name?",
+                default: "Mimikyu Cignetti"
+            },
+            {
+                type: "input",
+                name: "mgrEmail ",
+                message: "What is your employee Email?",
+                default: "mimikyu@fake.com"
+            },
+            {
+                type: "input",
+                name: "mgrId ",
+                message: "What is your employee ID?",
+                default: "9987"
+            },
+            {
+                type: "input",
+                name: "mgrOffice",
+                message: "What is your employee ID?",
+                default: "42"
+            },
+        ]
+        return managerQuestions
     }
-]
-module.exports = managerQuestions;
+
+//     internQuestions() {
+//         const internQuestions = [
+//             {
+//                 type: "confirm",
+//                 name: "startGame",
+//                 message: "Welcome to Team Bulider 9000 | Here we make a Webpage that Generates *YOUR* Team! | Do you want to try this App?",
+//             },
+//             {
+//                 type: "input",
+//                 name: "mgrName",
+//                 message: "Hello!, Intern What is your name?",
+//             }
+//         ]
+//         return internQuestions
+//     }
+}
+module.exports = Questions;
