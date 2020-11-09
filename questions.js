f25a340effcf385ca96ad1cb2dac50f76fad106e
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-09 10:37:02 -0500

    attempting to get questions working

--- questions.js
+++ questions.js
@@ -1,6 +1,14 @@
 const managerQuestions = [
     {
+        type: "confirm",
+        name: "startGame",
+        message: "Welcome to Team Bulider 9000 | Here we make a Webpage that Generates *YOUR* Team! | Do you want to try this App?",
+    },
+    {
         type: "input",
-        message: ""
+        name: "mgrName",
+        message: "Hello!, Manager What is your name?",
+    
     }
 ]
+module.exports = managerQuestions;
